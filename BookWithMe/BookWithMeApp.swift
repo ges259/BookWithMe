db1615c086e848719dd87f89f310643f92e9009b
chwl0571@daum.net <chwl0571@daum.net>
2025-05-04 21:44:54 +0900

    Initial Commit

---
+++ BookWithMe/BookWithMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWithMeApp.swift
+//  BookWithMe
+//
+//  Created by 계은성 on 5/4/25.
+//
+
+import SwiftUI
+
+@main
+struct BookWithMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
